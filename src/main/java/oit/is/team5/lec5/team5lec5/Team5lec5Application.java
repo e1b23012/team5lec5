5d8a4ace57cbeeebee436a145af24c2e48c28134
e1b23012 <e1b23012@oit.ac.jp>
2025-10-28 13:54:57 +0900

    firstbranch(^^)/

---
+++ src/main/java/oit/is/team5/lec5/team5lec5/Team5lec5Application.java
@@ -0,0 +1,13 @@
+package oit.is.team5.lec5.team5lec5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5lec5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5lec5Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team5/lec5/team5lec5/Team5lec5ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team5.lec5.team5lec5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5lec5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
